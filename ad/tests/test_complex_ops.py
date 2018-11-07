abf0be3f083dd35dc0e21e7b8c18e5d61675b4fb
Hugo Ramambason <hr365@cam.ac.uk>
2018-11-07 17:22:23 -0500

    update tests

--- ad/tests/test_complex_ops.py
+++ ad/tests/test_complex_ops.py
@@ -22,14 +22,14 @@ def test_tan_expression():
 def test_sinh_expression():
     a = ad.Variable('a')
     b = ad.Sinh(a)
-    assert b.eval({a: pi/4}) == 1
+    assert np.isclose(b.eval({a: pi/4}), 0.8686709614860095)
 
 def test_cosh_expression():
     a = ad.Variable('a')
     b = ad.Cosh(a)
-    assert b.eval({a: pi/4}) == 1
+    assert np.isclose(b.eval({a: pi/4}), 1.3246090892520057)
 
 def test_tanh_expression():
     a = ad.Variable('a')
     b = ad.Tanh(a)
-    assert b.eval({a: pi/4}) == 1
+    assert np.isclose(b.eval({a: pi/4}), 0.6557942026326724)
