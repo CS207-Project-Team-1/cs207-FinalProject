78197823d4826421c18f759f522cc0c4d24b6fee
Hugo Ramambason <hr365@cam.ac.uk>
2018-11-07 17:15:23 -0500

    new test

---
+++ ad/tests/test_complex_ops.py
@@ -0,0 +1,34 @@
+"""Tests for complex, one dimensional binary and unary operations"""
+import ad
+import pytest
+from math import pi
+
+def test_cosine_expression():
+    a = ad.Variable('a')
+    b = ad.Cos(a)
+    assert b.eval({a: pi/2}) == 0
+
+def test_sine_expression():
+    a = ad.Variable('a')
+    b = ad.Cos(a)
+    assert b.eval({a: pi/2}) == 1
+
+def test_tan_expression():
+    a = ad.Variable('a')
+    b = ad.Tan(a)
+    assert b.eval({a: pi/4}) == 1
+
+def test_sinh_expression():
+    a = ad.Variable('a')
+    b = ad.Sinh(a)
+    assert b.eval({a: pi/4}) == 1
+
+def test_cosh_expression():
+    a = ad.Variable('a')
+    b = ad.Cosh(a)
+    assert b.eval({a: pi/4}) == 1
+
+def test_tanh_expression():
+    a = ad.Variable('a')
+    b = ad.Tanh(a)
+    assert b.eval({a: pi/4}) == 1
