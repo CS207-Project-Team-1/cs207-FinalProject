2920dc92bb66aafe320c317201a338ea9ca2b390
Hugo Ramambason <hr365@cam.ac.uk>
2018-11-07 18:21:37 -0500

    update tests

--- ad/tests/test_complex_ops.py
+++ ad/tests/test_complex_ops.py
@@ -33,3 +33,25 @@ def test_tanh_expression():
     a = ad.Variable('a')
     b = ad.Tanh(a)
     assert np.isclose(b.eval({a: pi/4}), 0.6557942026326724)
+
+
+def test_variable_subtraction_derivative():
+    a, b = ad.Variable('a'), ad.Variable('b')
+    c = a - b
+    d = b - a
+    assert c.d({a:100, b:2}) == 0
+    assert d.d({a:10, b:20}) == 0
+
+ def test_variable_multiplication_derivative():
+    a, b = ad.Variable('a'), ad.Variable('b')
+    c = a * b
+    d = b * a
+    assert c.d({a:100, b:2}) == 102
+    assert d.d({a:10, b:20}) == 30
+
+ def test_variable_division():
+    a, b = ad.Variable('a'), ad.Variable('b')
+    c = a / b
+    d = b / a
+    assert c.d({a:100, b:2}) == -24.5
+    assert d.d({a:10, b:20}) == -0.1
