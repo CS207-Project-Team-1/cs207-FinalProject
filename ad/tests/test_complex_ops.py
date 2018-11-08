230ae9611c149fa9136e00df6b228842aa3a0470
Hugo Ramambason <hr365@cam.ac.uk>
2018-11-07 20:04:50 -0500

    addition test

--- ad/tests/test_complex_ops.py
+++ ad/tests/test_complex_ops.py
@@ -65,8 +65,8 @@ def test_variable_addition_derivative():
     a, b = ad.Variable('a'), ad.Variable('b')
     c = a + b
     d = b + a
-    assert c.d({a:100, b:2}) == 0
-    assert d.d({a:10, b:20}) == 0
+    assert c.d({a:100, b:2}) == 2
+    assert d.d({a:10, b:20}) == 2
 
 def test_variable_multiplication_derivative():
     a, b = ad.Variable('a'), ad.Variable('b')
