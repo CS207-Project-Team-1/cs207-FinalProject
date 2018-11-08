0b8e44a32d47e1d9577a6ae95946603961f2455e
Hugo Ramambason <hr365@cam.ac.uk>
2018-11-07 20:04:18 -0500

    test

--- ad/tests/test_complex_ops.py
+++ ad/tests/test_complex_ops.py
@@ -61,6 +61,13 @@ def test_variable_subtraction_derivative():
     assert c.d({a:100, b:2}) == 0
     assert d.d({a:10, b:20}) == 0
 
+def test_variable_addition_derivative():
+    a, b = ad.Variable('a'), ad.Variable('b')
+    c = a + b
+    d = b + a
+    assert c.d({a:100, b:2}) == 0
+    assert d.d({a:10, b:20}) == 0
+
 def test_variable_multiplication_derivative():
     a, b = ad.Variable('a'), ad.Variable('b')
     c = a * b
